526d4e0f81cdb5159621e12386dbe50b5764c16f
Rajesh-Khanna-ct <rajesh.chilaparasetty@cleartax.in>
2021-01-24 14:51:10 +0530

    p2p sample added

---
+++ drawingBoardFront/sample.js
@@ -0,0 +1,60 @@
+import p2pConnection from './modelJS/p2p.js';
+
+const sendMessage = (channel) => {
+    const message = document.querySelector('#messageText').value;
+    document.querySelector('#messageText').value = '';
+    if (message) {
+        channel.send(message);
+        const borad = document.querySelector('#messages');
+        const p = document.createElement('p');
+        p.setAttribute('class', 'myMessage');
+        p.innerText = `you: ${message}`;
+        borad.appendChild(p);
+    };
+}
+
+const handleMessage = (channel) => {
+    channel.onopen = (msg => {
+        document.querySelector('#HOSTPanel').style.display = 'none';
+        document.querySelector('#GUESTPanel').style.display = 'none';
+        document.querySelector('#messageBoard').style.display = 'block';
+    })
+    channel.onmessage = (message => {
+        const borad = document.querySelector('#messages');
+        const p = document.createElement('p');
+        p.setAttribute('class', 'singleMessage');
+        p.innerText = `other: ${message.data}`;
+        borad.appendChild(p);
+    })
+    document.querySelector('#sendMessage').addEventListener('click', () => { sendMessage(channel) });
+    document.querySelector('#messageText').addEventListener("keyup", function(event) {
+        if (event.keyCode === 13) {
+            sendMessage(channel);
+        }
+    });
+
+}
+
+const startHOST = () => {
+    console.log('starting host');
+    document.querySelector('#choice').style.display = 'none';
+    document.querySelector('#HOSTPanel').style.display = 'block';
+    const p2p = new p2pConnection(handleMessage);
+    p2p.makeCall();
+};
+
+const startGUEST = () => {
+    console.log('starting guest');
+    document.querySelector('#choice').style.display = 'none';
+    document.querySelector('#GUESTPanel').style.display = 'block';
+    const p2p = new p2pConnection(handleMessage);
+    p2p.receiveCall();
+};
+
+(() => {
+    document.querySelector('#HOSTPanel').style.display = 'none';
+    document.querySelector('#GUESTPanel').style.display = 'none';
+    document.querySelector('#messageBoard').style.display = 'none';
+    document.querySelector('#startHOST').addEventListener('click', startHOST);
+    document.querySelector('#startGUEST').addEventListener('click', startGUEST);
+})();
